59d5ed339ab022ff3264b3a1f3d5d488cf726f2c
Sarwat Jahan <159925204+SarwatJahan22@users.noreply.github.com>
2024-07-06 17:09:07 +0500

    Add files via upload

---
+++ Q12.js
@@ -0,0 +1,7 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+var names = ["Sarwat", "Zohaib", "Zayan", "Ayesha",];
+var message = "Do you like to play football?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
